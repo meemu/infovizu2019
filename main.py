a049816f7e2bbc3c3f5d5d793f09f22bd1979da1
meemu <teemu.nurminen@student.tut.fi>
2019-05-19 00:47:41 +0300

    New colors & sizes for the plot

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pandas as pd
 
 from bokeh.layouts import row, column
 from bokeh.models import Select, Slider
-from bokeh.palettes import Spectral5
 from bokeh.plotting import curdoc, figure
 
 
@@ -10,8 +9,8 @@ from bokeh.plotting import curdoc, figure
 # downloaded from https://www.kaggle.com/russellyates88/suicide-rates-overview-1985-to-2016
 df = pd.read_csv('master.csv')
 
-SIZES = list(range(6, 22, 3))
-COLORS = Spectral5
+SIZES = list(range(20, 60, 10))
+COLORS = list(['#FDE724', '#79D151', '#22A784', '#29788E', '#404387', '#440154'])
 N_SIZES = len(SIZES)
 N_COLORS = len(COLORS)
 
@@ -23,7 +22,6 @@ del df['HDI for year']
 # Fix the broken column name and remove commas
 df.rename(columns={' gdp_for_year ($) ': 'gdp_for_year ($)'},
           inplace=True)
-df['gdp_for_year ($)'] = df['gdp_for_year ($)'].astype(object)
 df["gdp_for_year ($)"] = df["gdp_for_year ($)"].str.replace(",","").astype(float)
 
 # Select the variables for the visulization
@@ -31,9 +29,6 @@ columns = sorted(df.columns)
 discrete = [x for x in columns if df[x].dtype == object]
 continuous = [x for x in columns if x not in discrete]
 continuous.remove('year')
-
-#continuous = list(['gdp_for_year ($)', 'gdp_per_capita ($)', 'suicides_no', 'population', 'suicides/100k pop'])
-#discrete = list(['year', 'age', 'country', 'sex'])
 
 # Here we set the dataframe to sum some of the columns, and exclude some of them
 summed = df.groupby(['country', 'year']).agg({'suicides_no': 'sum', 'population': 'sum', 'suicides/100k pop': 'sum', 'gdp_for_year ($)': 'min', 'gdp_per_capita ($)': 'min'})
@@ -94,10 +89,10 @@ x.on_change('value', update)
 y = Select(title='Y-Axis', value='gdp_for_year ($)', options=continuous)
 y.on_change('value', update)
 
-size = Select(title='Size', value='None', options=['None'] + continuous)
+size = Select(title='Size', value='suicides/100k pop', options=['None'] + continuous)
 size.on_change('value', update)
 
-color = Select(title='Color', value='None', options=['None'] + continuous)
+color = Select(title='Color', value='gdp_for_year ($)', options=['None'] + continuous)
 color.on_change('value', update)
 
 slider = Slider(start=1987, end=2015, value=2015, step=1, title='Year')
