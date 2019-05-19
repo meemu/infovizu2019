967f856525fb8b02097a1fa97498fdbbbc41881d
meemu <teemu.nurminen@student.tut.fi>
2019-05-19 15:58:15 +0300

    Add another plot and fillable text

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import pandas as pd
 
 from bokeh.layouts import row, column
-from bokeh.models import Select, Slider
+from bokeh.models import Select, Slider, HoverTool, Div, ColumnDataSource
 from bokeh.plotting import curdoc, figure
+
+
+
+
+# First block of text (before the first plot)
+text1 = Div(text="""Your <a href="https://en.wikipedia.org/wiki/HTML">HTML</a>-supported text is initialized with the <b>text</b> argument.  The
+remaining div arguments are <b>width</b> and <b>height</b>. For this example, those values
+are <i>200</i> and <i>100</i> respectively.""",
+width=1000, height=100)
+
+
+
 
 
 # Selecting the data from .csv
 # downloaded from https://www.kaggle.com/russellyates88/suicide-rates-overview-1985-to-2016
 df = pd.read_csv('master.csv')
 
-SIZES = list(range(20, 60, 10))
+SIZES = list(range(10, 50, 10))
 COLORS = list(['#FDE724', '#79D151', '#22A784', '#29788E', '#404387', '#440154'])
 N_SIZES = len(SIZES)
 N_COLORS = len(COLORS)
@@ -51,7 +63,13 @@ def create_figure():
         kw['y_range'] = sorted(set(ys))
     kw['title'] = "%s vs %s" % (x_title, y_title)
 
-    p = figure(plot_height=900, plot_width=1200, tools='save', **kw)
+    p = figure(plot_height=900,
+               plot_width=1200,
+               tools='save,box_zoom,reset',
+               min_border_left=100,
+               min_border_bottom=100,
+               min_border_right=20,
+               **kw)
     p.xaxis.axis_label = x_title
     p.yaxis.axis_label = y_title
 
@@ -80,8 +98,8 @@ def create_figure():
 
 
 def update(attr, old, new):
-    layout.children[1] = create_figure()
-
+    plot1.children[1] = create_figure()
+    
 
 x = Select(title='X-Axis', value='suicides/100k pop', options=continuous)
 x.on_change('value', update)
@@ -98,8 +116,80 @@ color.on_change('value', update)
 slider = Slider(start=1987, end=2015, value=2015, step=1, title='Year')
 slider.on_change('value', update)
 
-controls = column([x, y, color, size], width=200)
-layout = row(controls, create_figure(), slider)
 
+
+
+
+# Second block of text (after the first plot)
+text2 = Div(text="""Your <a href="https://en.wikipedia.org/wiki/HTML">HTML</a>-supported text is initialized with the <b>text</b> argument.  The
+remaining div arguments are <b>width</b> and <b>height</b>. For this example, those values
+are <i>200</i> and <i>100</i> respectively.""",
+width=1000, height=50)
+
+
+
+
+# The second plot
+# Selecting the data from .csv
+# downloaded from https://www.kaggle.com/russellyates88/suicide-rates-overview-1985-to-2016
+master = pd.read_csv('master.csv')
+columns2 = master[['country','year','sex','suicides_no']]
+finland = columns2[columns2['country'] == 'Finland']
+
+# Males by year
+males = finland[finland['sex'] == 'male']
+males_yearly = males.groupby('year').sum()
+males_ds = ColumnDataSource(males_yearly)
+
+# Females by year
+females = finland[finland['sex'] == 'female']
+females_yearly = females.groupby('year').sum()
+females_ds = ColumnDataSource(females_yearly)
+
+plot2 = figure(plot_height=500,
+           plot_width=1200,
+           h_symmetry=True,
+           x_axis_label='Year',
+           x_axis_type='linear',
+           x_axis_location='below',
+           x_range=(1987, 2015),
+           y_axis_label='Number of suicides',
+           y_axis_type='linear',
+           y_axis_location='left',
+           y_range=(0, 1300),
+           title='Suicide Rates in Finland from 1987 to 2016',
+           title_location='above',
+           tools='save',
+           min_border_left=100,
+           min_border_top=100,
+           min_border_bottom=20, 
+           min_border_right=20)
+
+plot2.grid.grid_line_color = None
+
+plot2.line(source=males_ds, x='year', y='suicides_no', color='#22A784',
+       line_width=2.5, legend='Males')
+
+plot2.line(source=females_ds, x='year', y='suicides_no', color='#404387',
+       line_width=2, legend='Females')
+
+plot2.add_tools(HoverTool(
+    tooltips=[
+        ( 'Year', '@year' ),
+        ( 'Suicides', '@suicides_no' )
+    ],
+
+    mode='vline'
+))
+
+
+
+# Setting up the HTML layout
+controls = column([x, y, color, size, slider], width=200)
+plot1 = row(create_figure(), controls)
+layout = column(text1, plot1, text2, plot2)
+
+
+# Displaying everything
 curdoc().add_root(layout)
 curdoc().title = "Suicides"
